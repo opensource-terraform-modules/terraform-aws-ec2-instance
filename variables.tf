607331be96ca9d12d60e0a08702d2f417e7c0d38
Yuriy Medvedev <support@opennix.ru>
2022-04-25 08:58:49 +0400

    deleted political slogans from repository

--- variables.tf
+++ variables.tf
@@ -299,8 +299,3 @@ variable "spot_valid_from" {
   default     = null
 }
 
-variable "putin_khuylo" {
-  description = "Do you agree that Putin doesn't respect Ukrainian sovereignty and territorial integrity? More info: https://en.wikipedia.org/wiki/Putin_khuylo!"
-  type        = bool
-  default     = true
-}
